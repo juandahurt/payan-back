9776f19076d88ea2a945ccafee2e6ad64eade967
Juan David Hurtado <juandahurtadom@gmail.com>
2022-04-28 15:48:22 -0500

    chore: remove comments

--- src/api/components/element/dal/element.dao.ts
+++ src/api/components/element/dal/element.dao.ts
@@ -4,7 +4,6 @@ import ElementSchema from "./element.schema";
 
 export class PYElementDAO implements PYElementDataAccessLogic {
     async getElementsByType(type: string): Promise<PYElement[]> {
-        console.log(typeof(type));
         let docs = await ElementSchema.find({});
         docs = docs.filter(doc => { return doc.type == type });
         return docs.map(doc => {
@@ -17,25 +16,5 @@ export class PYElementDAO implements PYElementDataAccessLogic {
                 doc.subtitle
             );
         });
-    }
-
-    async getElementsByTypeAndSubtype(type: string, subtype: String): Promise<PYElement[]> {
-        
-        return [];
-        // let docs = await ElementSchema.find({
-        //     type: type
-        // });
-        // docs = docs.filter(doc => { return doc.subtype == subtype });
-        // return docs.map(doc => {
-        //     return new PYElement(
-        //         doc._id,
-        //         doc.title,
-        //         doc.image,
-        //         doc.description,
-        //         doc.type,
-        //         doc.subtitle,
-        //         doc.subtype
-        //     );
-        // });
     }
 }
