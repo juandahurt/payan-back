ad110f4f99c28116a177f3a3fb87adcdb42a2e9a
Juan David Hurtado <juandahurtadom@gmail.com>
2022-04-28 15:49:49 -0500

    chore: remove old components

--- src/api/components/user/services/user.service.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../dal/user.model";
-import { UserDAO } from "../dal/user.dao";
-
-export class UserService {
-    async findByUsername(username: string): Promise<User | null> {
-        return new UserDAO().findByUsername(username);
-    }
-}
